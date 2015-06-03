8d0948abd23eba735f34763cc851ec2ab24f1931
Vivek Kumar <vivek.kumar1@markitserv.com>
2015-06-03 21:00:15 +0530

    Try git push

---
+++ MyWorld/app/src/main/java/com/myworld/MainActivity.java
@@ -0,0 +1,48 @@
+package com.myworld;
+
+import android.app.AlertDialog;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+
+public class MainActivity extends ActionBarActivity implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+    public void onClick(View v){}
+    public  void alertBtn(View v){
+      new AlertDialog.Builder(this)
+              .setTitle("Setting have been changed").setMessage("Want to modify the setting").setNeutralButton("ok",null).show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        int idMain =item.getGroupId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
